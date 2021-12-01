245322e364194738af8096d6ea8b12b83bbb6ccb
Max Gonzih <gonzih@gmail.com>
2021-12-01 09:33:35 -0800

    Day 1

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
-.PHONY: day1 day2
-day1 day2:
+TASKS = day1 day2
+
+.PHONY: all $(TASKS)
+$(TASKS):
 	cd $@ && python3 main.py
+all: $(TASKS)
--- day1/main.py
+++ day1/main.py
@@ -1,12 +1,45 @@
-prev = None
-counter = 0
+def first_part():
+    prev = None
+    counter = 0
 
-with open('input.txt') as input:
-    for line in input:
-        v = int(line)
-        if prev and prev < v:
+    with open('input.txt') as input:
+        for line in input:
+            v = int(line)
+            if prev and prev < v:
+                counter += 1
+                print(v)
+            prev = v
+
+    return counter
+
+
+def second_part():
+    numbers = []
+    windows = []
+    sums = []
+    counter = 0
+
+    with open('input.txt') as input:
+        for line in input:
+            v = int(line)
+            numbers.append(v)
+
+    for i in range(len(numbers)-2):
+        window = [numbers[i], numbers[i+1], numbers[i+2]]
+        windows.append(window)
+        sums.append(sum(window))
+
+    for i in range(len(sums)):
+        if i > 0 and sums[i-1] < sums[i]:
             counter += 1
-            print(v)
-        prev = v
 
-print(f"Answer is {counter}")
+    print(f"# of numbers {len(numbers)}")
+    print(f"# of windows {len(windows)}")
+    print(f"# of sums {len(sums)}")
+    print(f"Answer is {counter}")
+
+
+answer1 = first_part()
+answer2 = second_part()
+print(f"First answer is {answer1}")
+print(f"Second answer is {answer2}")
