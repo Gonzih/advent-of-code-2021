921170eac18490992eff1a0a7eace508dc7c679a
Max Gonzih <gonzih@gmail.com>
2021-12-02 22:36:57 -0800

    Ugh finish this tomorrow

--- day3/main.py
+++ day3/main.py
@@ -1,16 +1,25 @@
-import typing
+from typing import List
 
 
 def to_dec(binstr: str) -> int:
     return int(f"0b{binstr}", 2)
 
 
+def filter_by_pos(lines: List[str], pos: int, char: str):
+    return list(filter(lambda line: line[pos] == char, lines))
+
+
 class Report:
     def __init__(self):
         self.gamma_rate = 0
         self.epsilon_rate = 0
+        self.oxygen_rating = 0
+        self.co2_rating = 0
+        self.lines = []
         self.ones = []
         self.zeros = []
+        self.most_commons = []
+        self.least_commons = []
         self.allocated = False
         self.len = 0
 
@@ -22,6 +31,8 @@ class Report:
         self.len = n
 
     def process_line(self, line: str):
+        self.lines.append(line)
+
         if not self.allocated:
             self.allocate(len(line))
 
@@ -31,31 +42,52 @@ class Report:
             elif c == '1':
                 self.ones[i] += 1
 
-    def calculate(self):
-        gamma = []
-        epsilon = []
+    def calutate_power_consumption(self):
         for i in range(self.len):
             most_common = ''
             least_common = ''
 
-            if self.ones[i] > self.zeros[i]:
+            if self.ones[i] >= self.zeros[i]:
                 most_common = '1'
                 least_common = '0'
             else:
                 most_common = '0'
                 least_common = '1'
 
-            gamma.append(most_common)
-            epsilon.append(least_common)
+            self.most_commons.append(most_common)
+            self.least_commons.append(least_common)
 
-        self.gamma_rate = to_dec("".join(gamma))
-        self.epsilon_rate = to_dec("".join(epsilon))
+        self.gamma_rate = to_dec("".join(self.most_commons))
+        self.epsilon_rate = to_dec("".join(self.least_commons))
 
-        return True
+    def calculate_life_support_rating(self):
+        oxygen_ratings = self.lines
+        co2_ratings = self.lines
 
-    def answer(self) -> int:
-        self.calculate()
+        for i in range(self.len):
+            most = self.most_commons[i]
+            least = self.least_commons[i]
+
+            if len(oxygen_ratings) > 1:
+                oxygen_ratings = filter_by_pos(oxygen_ratings, i, most)
+            if len(co2_ratings) > 1:
+                co2_ratings = filter_by_pos(co2_ratings, i, least)
+
+            print(
+                f"Reduced oxy to {len(oxygen_ratings)}, co2 {len(co2_ratings)}")
+
+        self.oxygen_rating = to_dec("".join(oxygen_ratings[0]))
+        self.co2_rating = to_dec("".join(co2_ratings[0]))
+
+    def calculate(self):
+        self.calutate_power_consumption()
+        self.calculate_life_support_rating()
+
+    def first_answer(self) -> int:
         return self.gamma_rate * self.epsilon_rate
+
+    def second_answer(self) -> int:
+        return self.oxygen_rating * self.co2_rating
 
 
 def run():
@@ -67,7 +99,9 @@ def run():
             print(binstr)
             report.process_line(binstr)
 
-    print(f"First answer {report.answer()}")
+    report.calculate()
+    print(f"First answer {report.first_answer()}")
+    print(f"Second answer {report.second_answer()}")
 
 
 run()
