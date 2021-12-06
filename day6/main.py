9b0e7a71c74393843e8dfc79b9be36f431bf8eea
Max Gonzih <gonzih@gmail.com>
2021-12-06 09:43:08 -0800

    Saved by lil potato

--- day6/main.py
+++ day6/main.py
@@ -1,57 +1,30 @@
-from typing import Dict, List
+from typing import List
 
 
-def total(fish: Dict[int, int]) -> int:
-    result = 0
-    for k in fish:
-        result += fish[k]
-    return result
+def print_fish(fish: List[int]):
+    print("|".join([(str(v).rjust(3)) for v in fish]))
 
 
-def new_fish() -> Dict[int, int]:
-    fish = {8: 0}
-    for i in range(8):
-        fish[i] = 0
-    return fish
-
-
-def print_fish(fish: Dict[int, int]):
-    out: List[str] = []
-    for k in range(9):
-        v = str(fish[k])
-        out.append(f"{v.rjust(3)}")
-    print("|".join(out))
-
-# def set_or_init(fish: Dict[int, int], newv: int, init: v):
-
-
-def count_fish(fish: Dict[int, int], days: int) -> int:
+def count_fish(fish: List[int], days: int) -> int:
     for i in range(days):
         print_fish(fish)
-        new_generation = new_fish()
-        newborns = fish[0]
-        for k in range(9):
-            if k == 0:
-                new_generation[6] = fish[k]
-            else:
-                new_generation[k-1] += fish[k]
-        new_generation[8] += newborns
-        fish = new_generation
-    return total(fish)
+        newborns = fish.pop(0)
+        fish.append(newborns)
+        fish[6] += newborns
+    return sum(fish)
 
 
 def run():
-    fish = new_fish()
+    fish = [0 for _ in range(9)]
     print_fish(fish)
 
     with open('input.txt') as input:
         for line in input:
-            initial_fish = [int(s) for s in line.strip().split(',')]
-            for age in initial_fish:
+            init = [int(s) for s in line.strip().split(',')]
+            for age in init:
                 fish[age] += 1
 
-    d0 = total(fish)
-    print(f"Starting with {d0} jellyfishes")
+    d0 = sum(fish)
     assert(d0 == 300)
 
     d80 = count_fish(fish.copy(), 80)
@@ -60,7 +33,7 @@ def run():
 
     d256 = count_fish(fish.copy(), 256)
     print(f"d256 {d256}")
-    assert(d256 >= 375482)
+    assert(d256 == 1689540415957)
 
 
 run()
