1203b7e7d9c5b81102d5a189594feaf1cfdeb6dc
Max Gonzih <gonzih@gmail.com>
2021-12-10 20:05:46 -0800

    Setup template

--- template-rs/src/main.rs
+++ template-rs/src/main.rs
@@ -1,7 +1,36 @@
-fn main() {
+use std::io::Result;
+
+#[derive(Debug)]
+struct Line {
+    line: &'static str,
+}
+
+impl Line {
+    fn new(line: &'static str) -> Line {
+        Line { line }
+    }
+}
+
+impl Line {
+    fn process(&self) -> Result<()> {
+        println!("{}", self.line);
+        Ok(())
+    }
+}
+
+fn run(input: &'static str) -> Result<()> {
+    input
+        .lines()
+        .map(|line| Line::new(line).process())
+        .collect()
+}
+
+fn main() -> Result<()> {
     let test_input = include_str!("test-input.txt");
     let input = include_str!("input.txt");
-    println!("{}", test_input);
-    println!("{}", input);
-    println!("Hello, world!");
+
+    run(test_input)?;
+    run(input)?;
+
+    Ok(())
 }
